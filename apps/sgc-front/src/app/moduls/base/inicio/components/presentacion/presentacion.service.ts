9f5bf762d8f2622f649a40a914e206378d14626d
lobo_este_pario <lobo_este_pario@hotmail.com>
2024-03-03 16:35:29 -0600

    .

--- apps/sgc-front/src/app/moduls/base/inicio/components/presentacion/presentacion.service.ts
+++ apps/sgc-front/src/app/moduls/base/inicio/components/presentacion/presentacion.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class PresentacionService implements HttpInterceptor{
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
